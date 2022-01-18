5450919bab3eb3d3efe4de4c86509478cbf79b45
Oleg Lavrovsky <oleg@datalets.ch>
2022-01-18 22:18:47 +0100

    Attribution

---
+++ src/WA_Other_Furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="WA_Other_Furniture" tilewidth="32" tileheight="32" tilecount="156" columns="12">
+ <image source="assets/WA_Other_Furniture.png" width="384" height="416"/>
+</tileset>
---
+++ src/WA_Seats.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="WA_Seats" tilewidth="32" tileheight="32" tilecount="182" columns="13">
+ <image source="assets/WA_Seats.png" width="416" height="448"/>
+</tileset>
