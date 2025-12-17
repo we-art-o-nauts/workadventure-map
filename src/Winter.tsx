b85d7a944921542eefaafd8b9a1124d30050fbd1
Oleg Lavrovsky <loleg@users.noreply.github.com>
2025-12-17 23:24:10 +0100

    Swiss chalet

---
+++ src/Winter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="winter-tileset-db32" tilewidth="32" tileheight="32" tilecount="15" columns="5">
+ <image source="assets/winter-tileset-db32.png" width="160" height="96"/>
+</tileset>
